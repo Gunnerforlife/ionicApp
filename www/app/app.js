70a9672d2a3524852daafd99c734cf6391104c1d
Gunnerforlife <arun@twamplify.com>
2014-12-09 16:07:23 +0530

    layout of the home page set

---
+++ www/app/app.js
@@ -0,0 +1,29 @@
+/**
+ * Created by persi52 on 9/12/14.
+ */
+angular.module("ionicApp",["ionic"])
+
+    .run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if(window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if(window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    })
+
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+
+            .state('home', {
+                url: "/home",
+                templateUrl: "app/home/home.html"
+                //controller: 'AppCtrl'
+            })
+        $urlRouterProvider.otherwise('/home');
+    });
